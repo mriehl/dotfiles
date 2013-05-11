144f80a1788e3e8c9661fc05661302235be5fb22
mriehl <maximilien.riehl@gmail.com>
2013-05-11 18:37:57 +0200

    adding sublime text 3 stuff

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ function install () {
     TARGET=$1
     FILE=`realpath $2`
     echo "Installing $FILE at $TARGET"
+    [[ ! -d `dirname $TARGET` ]] && {
+        echo "  creating upper directories.."
+        mkdir -p `dirname $TARGET`
+    }
     [[ -f $TARGET ]] && {
         echo "  $TARGET exists, removing.."
         rm -f $TARGET
@@ -21,3 +25,10 @@ function install () {
 install ~/.vimrc .vimrc
 install ~/.xmobarrc .xmobarrc
 install ~/.xmonad .xmonad
+
+install ~/.config/sublime-text-3/Packages/User/coroutine.sublime-snippet sublime/coroutine.sublime-snippet
+install ~/.config/sublime-text-3/Packages/User/decorate_coroutine.sublime-snippet sublime/decorate_coroutine.sublime-snippet
+install ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings sublime/Preferences.sublime-settings
+install ~/.config/sublime-text-3/Packages/User/pyb-venv.sublime-build sublime/pyb-venv.sublime-build
+install ~/.config/sublime-text-3/Packages/User/python-single-file.sublime-build sublime/python-single-file.sublime-build
+

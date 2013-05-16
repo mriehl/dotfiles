fd092a68664dcba539fee0e1c9ded408de11b466
mriehl <maximilien.riehl@gmail.com>
2013-05-16 13:02:29 +0200

    build system for packaging maven projects, must use bash for java environment magic :-(

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ function install () {
 
     [[ "$AS_ROOT" == "as_root" ]] && sudo ln -s $FILE $TARGET && echo "  Installed $TARGET as root"
     [[ "$AS_ROOT" == "as_root" ]] || ln -s $FILE $TARGET && echo "  Installed $TARGET"
-    
+
 }
 
 install ~/.vimrc .vimrc
@@ -36,6 +36,7 @@ install ~/.config/sublime-text-3/Packages/User/coroutine.sublime-snippet sublime
 install ~/.config/sublime-text-3/Packages/User/decorate_coroutine.sublime-snippet sublime/decorate_coroutine.sublime-snippet
 install ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings sublime/Preferences.sublime-settings
 install ~/.config/sublime-text-3/Packages/User/pyb-venv.sublime-build sublime/pyb-venv.sublime-build
+install ~/.config/sublime-text-3/Packages/User/mvn_package.sublime-build sublime/mvn_package.sublime-build
 install ~/.config/sublime-text-3/Packages/User/python-single-file.sublime-build sublime/python-single-file.sublime-build
 install ~/.config/sublime-text-3/Packages/User/python-single-file-with-venv.sublime-build sublime/python-single-file-with-venv.sublime-build
 

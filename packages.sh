58817ab943e6b2220ca65f1bffb8aef58e4ed375
mriehl <maximilien.riehl@gmail.com>
2013-06-30 12:52:04 +0200

    installing C runtime libs for juniper network connect

--- packages.sh
+++ packages.sh
@@ -8,6 +8,6 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 
 ## misc packages
-sudo apt-get install oracle-java7-installer realpath acpi xbacklight python-virtualenv xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage dropbox tree
+sudo apt-get install oracle-java7-installer realpath acpi xbacklight python-virtualenv xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage dropbox tree libc6:i386 zlib1g:i386
 
 

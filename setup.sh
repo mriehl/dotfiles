faa61dafdf638129fb31525139a80e5c897fe909
mriehl <maximilien.riehl@gmail.com>
2013-08-30 11:20:19 +0200

    more pleasant output

--- setup.sh
+++ setup.sh
@@ -2,12 +2,14 @@
 set -u -e -E -C -o pipefail
 
 function install_required_libraries() {
+    echo "-checking availability of required libraries"
     command -v realpath >>/dev/null 2>&1 || sudo apt-get install realpath
 }
 
 function install_source_code_pro() {
+    echo "-checking SourceCodePro installation"
     find ~/.fonts -name "*SourceCodePro*" | grep -q '.' || {
-        echo "SourceCodePro not installed, installing.."
+        echo "--SourceCodePro not installed, installing"
         FONT_NAME="SourceCodePro"
         URL="http://sourceforge.net/projects/sourcecodepro.adobe/files/latest/download"
 
@@ -28,48 +30,54 @@ function install () {
     AS_ROOT=${3:-''}
     GAINROOT=""
     [[ "$AS_ROOT" == "as_root" ]] && GAINROOT="sudo "
-    echo "Installing $FILE at $TARGET"
     [[ ! -d `dirname "$TARGET"` ]] && {
-        echo "  creating upper directories.."
         $GAINROOT mkdir -p `dirname $TARGET`
     }
     [[ -d $TARGET || -L $TARGET || -f $TARGET ]] && {
-        echo "  $TARGET exists, removing.."
         $GAINROOT rm -r "$TARGET"
     }
 
-    $GAINROOT ln -s "$FILE" "$TARGET" && echo "  Installed $TARGET $AS_ROOT"
+    $GAINROOT ln -s "$FILE" "$TARGET"
 
 }
 
 install_required_libraries
 
+echo "-installing dotfiles"
+
 #---rcfiles
+echo "--installing rcfiles"
+echo "---vim"
 install ~/.vimrc rcfiles/.vimrc
+echo "---xmonad"
 install ~/.xmobarrc rcfiles/.xmobarrc
 install ~/.xmonad rcfiles/.xmonad
+echo "---git"
 install ~/.gitconfig rcfiles/.gitconfig
 
 #---sublime text
+echo "--installing ST3 config"
 #------snippets
+echo "---snippets"
 install ~/.config/sublime-text-3/Packages/User/coroutine.sublime-snippet sublime/coroutine.sublime-snippet
 install ~/.config/sublime-text-3/Packages/User/decorate_coroutine.sublime-snippet sublime/decorate_coroutine.sublime-snippet
-#------settings
+echo "---settings"
 install ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings sublime/Preferences.sublime-settings
 install ~/.config/sublime-text-3/Packages/User/Anaconda.sublime-settings sublime/Anaconda.sublime-settings
-#------keybindings
+echo "---keybindings"
 install ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap sublime/Default\ \(Linux\).sublime-keymap
-#------build systems
-#---------python
+echo "---build systems"
+echo "----python"
 install ~/.config/sublime-text-3/Packages/User/pyb-venv.sublime-build sublime/pyb-venv.sublime-build
 install ~/.config/sublime-text-3/Packages/User/python-single-file.sublime-build sublime/python-single-file.sublime-build
 install ~/.config/sublime-text-3/Packages/User/python-single-file-with-venv.sublime-build sublime/python-single-file-with-venv.sublime-build
-#---------java
+echo "----java"
 install ~/.config/sublime-text-3/Packages/User/mvn.sublime-build sublime/mvn.sublime-build
-#---------js
+echo "----js"
 install ~/.config/sublime-text-3/Packages/User/nodejs-single-file.sublime-build sublime/nodejs-single-file.sublime-build
 
 #---scripts
+echo "--installing scripts"
 install /usr/local/bin/statusbar statusbar as_root
 install /usr/local/bin/xmonad-start xmonad-start as_root
 install /usr/local/bin/xmobar-change-screen xmobar-change-screen as_root

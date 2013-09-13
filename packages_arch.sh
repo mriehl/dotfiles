db5d4d8dfd08d3925d96392c4880e46e9dc59dc9
mriehl <maximilien.riehl@gmail.com>
2013-09-13 10:56:48 +0200

    adding package tuning script for arch

---
+++ packages_arch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo pacman -Sy
+sudo pacman -S tmux yaourt vim python-pip python2-pip traceroute ruby ruby-ronn gcc make jdk7-openjdk acpi tree nmap git subversion
+
+sudo pip install virtualenv
+
+# avoid clashing keybindings with parcellite by removing it
+sudo pacman -R parcellite

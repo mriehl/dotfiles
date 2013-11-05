27c1bec83c7d16bc5024bd1570b3fd34bef4d5c0
mriehl <maximilien.riehl@gmail.com>
2013-11-05 14:50:13 +0100

    using tabs in go syntax

--- setup.sh
+++ setup.sh
@@ -69,6 +69,7 @@ echo "---settings"
 install ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings sublime/Preferences.sublime-settings
 install ~/.config/sublime-text-3/Packages/User/Anaconda.sublime-settings sublime/Anaconda.sublime-settings
 install ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings sublime/Package\ Control.sublime-settings
+install ~/.config/sublime-text-3/Packages/User/GoSublime-Go.sublime-settings sublime/GoSublime-Go.sublime-settings
 echo "---keybindings"
 install ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap sublime/Default\ \(Linux\).sublime-keymap
 echo "---build systems"

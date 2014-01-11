5a6493e19bc194879544d1b9feb0d92feaf13bd6
mriehl <max@riehl.io>
2014-01-11 12:38:15 +0100

    adding xmonad-contrib and xmobar for arch setup

--- packages_arch.sh
+++ packages_arch.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 sudo pacman -Sy
-sudo pacman -S tmux yaourt vim python-pip python2-pip traceroute ruby gcc make jdk7-openjdk acpi tree nmap git subversion
+sudo pacman -S tmux yaourt vim python-pip python2-pip traceroute ruby gcc make jdk7-openjdk acpi tree nmap git subversion xmonad-contrib xmobar
 
 sudo yaourt -Sy
-sudo yaourt -S ruby-ronn
 
-sudo pip install virtualenv
+sudo pip install --upgrade virtualenv
 
 # avoid clashing keybindings with parcellite by removing it
 sudo pacman -R parcellite

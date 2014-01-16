e44d74c77c878cbad1e155679b3d2ab320cfda4f
mriehl <max@riehl.io>
2014-01-16 10:17:29 +0100

    zsh is now a dependency

--- packages_arch.sh
+++ packages_arch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -Sy
-sudo pacman -S tmux yaourt vim python-pip python2-pip traceroute ruby gcc make jdk7-openjdk acpi tree nmap git subversion xmonad-contrib xmobar chromium
+sudo pacman -S tmux yaourt vim python-pip python2-pip traceroute ruby gcc make jdk7-openjdk acpi tree nmap git subversion xmonad-contrib xmobar chromium zsh
 
 sudo yaourt -Sy
 

ea9733ee12266c9867285f5189b12779eba6363a
mriehl <max@riehl.io>
2014-01-16 10:16:41 +0100

    zsh is now a dependency

--- packages_debian.sh
+++ packages_debian.sh
@@ -8,4 +8,4 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 
 ## misc packages
-sudo apt-get install oracle-java7-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage dropbox tree libc6:i386 zlib1g:i386 nmap synapse
+sudo apt-get install oracle-java7-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage dropbox tree libc6:i386 zlib1g:i386 nmap synapse zsh

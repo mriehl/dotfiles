fe16bd7ac9092844c9f364446e8d93430860bf1a
mriehl <max@riehl.io>
2014-01-19 18:44:22 +0100

    predefine function i use very often to look at stuff in the interpreter

--- rcfiles/.pythonrc.py
+++ rcfiles/.pythonrc.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 try:
     import readline
 except ImportError:
@@ -5,3 +7,12 @@ except ImportError:
 else:
     import rlcompleter
     readline.parse_and_bind("tab: complete")
+
+import inspect
+
+
+def what(stuff):
+    for k, v in inspect.getmembers(stuff):
+        print("* %s" % k)
+        for line in str(v).splitlines():
+            print("\t%s" % line)

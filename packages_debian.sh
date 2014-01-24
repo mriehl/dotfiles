dcc1680c3a8516471149f26c7a9ff67f7eb33c6f
mriehl <max@riehl.io>
2014-01-24 15:31:50 +0100

    require asciinema pypi package

--- packages_debian.sh
+++ packages_debian.sh
@@ -9,3 +9,5 @@ sudo apt-get update
 
 ## misc packages
 sudo apt-get install oracle-java7-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage dropbox tree libc6:i386 zlib1g:i386 nmap synapse zsh
+
+sudo pip install --upgrade asciinema

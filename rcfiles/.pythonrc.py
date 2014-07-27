0279bd99fd2c9681774a3260879fbf0e3e7d95bb
mriehl <max@riehl.io>
2014-07-27 10:16:59 +0200

    use jedi in shell for awesome completions

--- rcfiles/.pythonrc.py
+++ rcfiles/.pythonrc.py
@@ -1,14 +1,21 @@
 from __future__ import print_function
 
-try:
-    import readline
-except ImportError:
-    pass
-else:
-    import rlcompleter
-    readline.parse_and_bind("tab: complete")
-
 import inspect
+
+
+try:
+    from jedi.utils import setup_readline
+    setup_readline()
+except ImportError:
+    # Fallback to the stdlib readline completer if it is installed.
+    # Taken from http://docs.python.org/2/library/rlcompleter.html
+    print("Jedi is not installed, falling back to readline")
+    try:
+        import readline
+        import rlcompleter
+        readline.parse_and_bind("tab: complete")
+    except ImportError:
+        print("Readline is not installed either. No tab completion is enabled.")
 
 
 def what(stuff, predicate=None):

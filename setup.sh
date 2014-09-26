2c4d8e45601271587ad7553af96f33e08a887caa
mriehl <max@riehl.io>
2014-09-26 20:34:50 +0000

    racer and rust.vim plugins

--- setup.sh
+++ setup.sh
@@ -63,6 +63,8 @@ echo "----pathogen plugins"
 install ~/.vim/bundle/jedi-vim rcfiles/.vim/bundle/jedi-vim/
 install ~/.vim/bundle/nerdtree rcfiles/.vim/bundle/nerdtree/
 install ~/.vim/bundle/ctrlp.vim rcfiles/.vim/bundle/ctrlp.vim/
+install ~/.vim/bundle/rust.vim rcfiles/.vim/bundle/rust.vim/
+install ~/.vim/bundle/racer/autoload/racer.vim rcfiles/.vim/bundle/racer/editors/racer.vim
 echo "---xmonad"
 install ~/.xmobarrc rcfiles/.xmobarrc
 install ~/.xmonad rcfiles/.xmonad

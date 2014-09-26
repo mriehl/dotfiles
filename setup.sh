b48e8316843d7b4214baab9628816d67d182af71
mriehl <max@riehl.io>
2014-09-26 14:57:26 +0200

    add nerdtree

--- setup.sh
+++ setup.sh
@@ -40,6 +40,7 @@ install ~/.vimrc rcfiles/.vimrc
 install ~/.vim/autoload/pathogen.vim rcfiles/.vim/autoload/pathogen.vim
 echo "----pathogen plugins"
 install ~/.vim/bundle/jedi-vim rcfiles/.vim/bundle/jedi-vim/
+install ~/.vim/bundle/nerdtree rcfiles/.vim/bundle/nerdtree/
 echo "---xmonad"
 install ~/.xmobarrc rcfiles/.xmobarrc
 install ~/.xmonad rcfiles/.xmonad

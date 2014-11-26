6166f8751240bebb6502c66110ae7d2f060fcfe5
mriehl <max@riehl.io>
2014-11-26 09:50:32 +0100

    set X background with feh

--- packages_debian.sh
+++ packages_debian.sh
@@ -8,4 +8,4 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 
 ## misc packages
-sudo apt-get install oracle-java8-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage dropbox tree firefox libc6:i386 zlib1g:i386 nmap synapse zsh vim fish i3
+sudo apt-get install oracle-java8-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage dropbox tree firefox libc6:i386 zlib1g:i386 nmap synapse zsh vim fish i3 feh

0954ee86fa384db66cc24bc52b6cad71b1cc963c
mriehl <max@riehl.io>
2014-11-27 16:48:21 +0000

    no need to use yaourt

--- packages_arch.sh
+++ packages_arch.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 sudo pacman -Sy
-sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh
+sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh terminology
 
 yaourt -Sy
-yaourt -S terminology
 
 sudo pip install --upgrade virtualenv
 

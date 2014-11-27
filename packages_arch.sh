f4f7cde2e99abd97a39c93e689c28d782af50ad9
mriehl <max@riehl.io>
2014-11-27 08:55:34 +0100

    terminology rules

--- packages_arch.sh
+++ packages_arch.sh
@@ -4,6 +4,7 @@ sudo pacman -Sy
 sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh
 
 yaourt -Sy
+yaourt -S terminology
 
 sudo pip install --upgrade virtualenv
 
--- packages_debian.sh
+++ packages_debian.sh
@@ -2,10 +2,11 @@
 
 ## oracle java PPA
 sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:niko2040/e19 # terminology
 
 
 ## fetch current packages
 sudo apt-get update
 
 ## misc packages
-sudo apt-get install oracle-java8-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage dropbox tree firefox libc6:i386 zlib1g:i386 nmap synapse zsh vim fish i3 feh
+sudo apt-get install oracle-java8-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage dropbox tree firefox libc6:i386 zlib1g:i386 nmap synapse zsh vim fish i3 feh terminology

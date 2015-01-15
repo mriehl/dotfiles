27cf75eddbcff36c09d043c3058dff1234a48a98
mriehl <max@riehl.io>
2015-01-15 12:16:38 +0100

    install light table launcher

--- setup.sh
+++ setup.sh
@@ -112,6 +112,7 @@ install /usr/local/bin/xmobar-change-screen bin/xmobar-change-screen as_root
 install /usr/local/bin/pip-upgrade-all bin/pip-upgrade-all as_root
 install /usr/local/bin/go-upgrade bin/go-upgrade as_root
 install /usr/local/bin/pycharm bin/pycharm as_root
+install /usr/local/bin/light-table bin/light-table as_root
 install /usr/local/bin/pwn-docker bin/pwn-docker as_root
 install /usr/local/bin/render-wireless bin/render_wireless as_root
 install /usr/local/bin/passme bin/passme as_root

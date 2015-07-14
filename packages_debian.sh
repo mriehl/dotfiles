dcc32da1306a48c3300ec525e9db826aa6e4239c
mriehl <max@riehl.io>
2015-07-14 08:42:03 +0200

    debian dependency updates

--- packages_debian.sh
+++ packages_debian.sh
@@ -9,4 +9,4 @@ sudo add-apt-repository ppa:niko2040/e19 # terminology
 sudo apt-get update
 
 ## misc packages
-sudo apt-get install oracle-java8-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage tree firefox libc6:i386 zlib1g:i386 nmap zsh vim fish i3 i3blocks feh terminology
+sudo apt-get install oracle-java8-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage tree firefox libc6:i386 zlib1g:i386 nmap zsh vim i3 i3blocks feh terminology imagemagick scrot emacs

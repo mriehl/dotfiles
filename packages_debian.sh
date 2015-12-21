01b7c77bba9a399d1980abd31d9fbecab4c3583f
mriehl <max@riehl.io>
2015-12-21 14:48:26 +0100

    tmux dependency

--- packages_debian.sh
+++ packages_debian.sh
@@ -9,4 +9,4 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 
 ## misc packages
-sudo apt-get install oracle-java8-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage tree firefox libc6:i386 zlib1g:i386 nmap zsh vim i3 i3blocks feh terminology imagemagick scrot emacs tig neovim
+sudo apt-get install oracle-java8-installer realpath acpi xbacklight xmonad xmobar dmenu python-dev curl arandr vim htop git python-pip subversion trayer xloadimage tree firefox libc6:i386 zlib1g:i386 nmap zsh vim i3 i3blocks feh terminology imagemagick scrot emacs tig neovim tmux

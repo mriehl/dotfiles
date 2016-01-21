783431c49e315363bc3e54bc320d1403ff0ee058
mriehl <max@riehl.io>
2016-01-21 09:16:16 +0100

    remove annoying menu bar

--- rcfiles/emacs.d/init.el
+++ rcfiles/emacs.d/init.el
@@ -1,6 +1,8 @@
 (setq user-full-name "Maximilien Riehl")
 (setq user-mail-address "max@riehl.io")
 
+(menu-bar-mode -1)
+
 (add-to-list 'load-path (expand-file-name "subconfigs" user-emacs-directory))
 (require 'init-packages)
 (require 'init-evil)

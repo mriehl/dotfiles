6d07a8230964cf09bb301ac8a72451b279666253
mriehl <max@riehl.io>
2016-01-25 08:35:37 +0100

    ret and tab settings

--- rcfiles/emacs.d/init.el
+++ rcfiles/emacs.d/init.el
@@ -12,3 +12,30 @@
 (require 'init-theme)
 (require 'init-helm)
 (require 'init-powerline)
+
+;; esc quits
+(defun minibuffer-keyboard-quit ()
+    "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+    (interactive)
+    (if (and delete-selection-mode transient-mark-mode mark-active)
+	(setq deactivate-mark  t)
+      (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+      (abort-recursive-edit)))
+
+
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; indent on ret
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+(global-set-key [escape] 'evil-exit-emacs-state)

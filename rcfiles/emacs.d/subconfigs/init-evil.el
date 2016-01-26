24e355d899842bde1520bb4884ba02889b8f7e72
mriehl <max@riehl.io>
2016-01-26 08:33:09 +0100

    evil settings

--- rcfiles/emacs.d/subconfigs/init-evil.el
+++ rcfiles/emacs.d/subconfigs/init-evil.el
@@ -2,7 +2,22 @@
 
 (setq evil-want-C-i-jump nil) ; org mode tab fix
 (require 'evil)
+
+(setq evil-search-module 'evil-search)
+(setq evil-magic 'very-magic)
+
 (evil-mode 1)
 
-(provide 'init-evil)
+(require-package 'evil-surround)
+(require 'evil-surround)
+(global-evil-surround-mode 1)
 
+(require-package 'evil-matchit)
+(require 'evil-matchit)
+(global-evil-matchit-mode 1)
+
+(require-package 'evil-visualstar)
+(require 'evil-visualstar)
+(global-evil-visualstar-mode t)
+
+(provide 'init-evil)

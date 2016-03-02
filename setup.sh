37780a2d5be45877c27ddc2ee97049fee63c7c3e
mriehl <max@riehl.io>
2016-03-02 09:12:10 +0100

    new font version

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ function install_source_code_pro() {
     find ~/.fonts -name "*SourceCodePro*" | grep -q '.' || {
         echo "--SourceCodePro not installed, installing"
         FONT_NAME="SourceCodePro"
-        URL="https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip"
+        URL="https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip"
 
         mkdir  /tmp/adobefont
         cd /tmp/adobefont

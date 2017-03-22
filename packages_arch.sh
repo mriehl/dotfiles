3611ed35060b4e6fc18539985e80f5544027f699
mriehl <max@flatmap.ninja>
2017-03-22 16:51:34 +0100

    updates on manjaro setup

--- packages_arch.sh
+++ packages_arch.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 sudo pacman -Sy
-sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh terminology
+sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh terminology gnome-terminal arandr dmenu emacs tig
 
 yaourt -Sy
+sudo yaourt -S i3blocks
 
 sudo pip install --upgrade virtualenv
 

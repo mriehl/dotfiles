ba07a9827cbd03800f6d50895e45454ecf978a35
mriehl <max@flatmap.ninja>
2017-03-23 09:37:53 +0100

    arch setup updates

--- packages_arch.sh
+++ packages_arch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -Sy
-sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh terminology gnome-terminal arandr dmenu emacs tig sbt scala docker network-manager-applet
+sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh terminology gnome-terminal arandr dmenu emacs tig sbt scala docker network-manager-applet hub
 
 yaourt -Sy
 sudo yaourt -S i3blocks

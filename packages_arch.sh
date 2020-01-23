96789e03c12db59e66a27c0357940f948ef1874b
Maximilien Riehl <max@riehl.io>
2020-01-23 14:57:54 +0100

    [fix] some tweaks from recent setup

--- packages_arch.sh
+++ packages_arch.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
 sudo pacman -Sy
-sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh terminology gnome-terminal arandr dmenu emacs tig sbt scala docker network-manager-applet hub hunspell-de hunspell-en hunspell-fr bind-tools
+sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh terminology gnome-terminal arandr dmenu emacs tig sbt scala docker network-manager-applet hub hunspell-de hunspell-en_US bind-tools rustup xsel keybase keybase-gui mosh firefox xorg-xsetroot networkmanager network-manager-applet xautolock htop xorg-xbacklight i3blocks
 
-yaourt -Sy
-sudo yaourt -S i3blocks
-
-sudo pip install --upgrade virtualenv
-
-# avoid clashing keybindings with parcellite by removing it
-sudo pacman -R parcellite
+sudo aura -A i3blocks-contrib githud
 
 sudo archlinux-java set java-8-openjdk

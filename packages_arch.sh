a317f83fd9a94e420297359c25e6e73f2d7aebf1
Maximilien Riehl <max@riehl.io>
2020-09-07 19:08:22 +0200

    move to pacman -Qe for packages for now

--- packages_arch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo pacman -Sy
-sudo pacman -S tmux yaourt python-pip python2-pip traceroute ruby gcc make jdk8-openjdk acpi tree nmap git subversion xmonad-contrib xmobar firefox zsh intel-ucode trayer srm fish vim-python3 i3-wm i3lock i3status scrot imagemagick feh terminology gnome-terminal arandr dmenu emacs tig sbt scala docker network-manager-applet hub hunspell-de hunspell-en_US bind-tools rustup xsel keybase keybase-gui mosh firefox xorg-xsetroot networkmanager network-manager-applet xautolock htop xorg-xbacklight i3blocks
-
-sudo aura -A i3blocks-contrib githud
-
-sudo archlinux-java set java-8-openjdk

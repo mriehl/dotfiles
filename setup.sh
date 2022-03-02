95041d01230deb2b5d814fcf36ce9fa76f294880
Maximilien Riehl <max@riehl.io>
2022-03-02 08:23:28 +0100

    [chore] cleanup before modernization

--- setup.sh
+++ setup.sh
@@ -13,23 +13,6 @@ function check_required_libraries() {
     [ -z ${REALPATH+x} ] && REALPATH="readlink -f" || :
 }
 
-function install_source_code_pro() {
-    echo "-checking SourceCodePro installation"
-    find ~/.fonts -name "*SourceCodePro*" | grep -q '.' || {
-        echo "--SourceCodePro not installed, installing"
-        FONT_NAME="SourceCodePro"
-        URL="https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip"
-
-        mkdir  /tmp/adobefont
-        cd /tmp/adobefont
-        wget ${URL} -O ${FONT_NAME}.zip
-        unzip -o -j ${FONT_NAME}.zip
-        mkdir -p ~/.fonts
-        cp *.otf.woff ~/.fonts
-        fc-cache -f -v
-    }
-}
-
 function install () {
     TARGET=$1
     FILE=$($REALPATH "$2")
@@ -58,35 +41,16 @@ echo "-installing dotfiles"
 
 #---rcfiles
 echo "--installing rcfiles"
-echo "---sbt"
-install ~/.sbt/0.13/global.sbt rcfiles/global.sbt
-install ~/.sbt/0.13/plugins/plugins.sbt rcfiles/plugins.sbt
-echo "---mjolnir"
-install ~/.mjolnir rcfiles/mjolnir
-install ~/.hammerspoon rcfiles/hammerspoon
 echo "---zsh"
 install ~/.zshrc rcfiles/.zshrc
 install ~/.zshrc.local rcfiles/.zshrc.local
-echo "---fish"
-install ~/.config/fish/config.fish rcfiles/fish/config.fish
-echo "---vim"
-install ~/.config/nvim/init.vim rcfiles/.vimrc
-install ~/.config/nvim/autoload/plug.vim rcfiles/.vim/autoload/plug.vim
-echo "----pathogen plugins"
-install ~/.config/nvim/bundle rcfiles/.vim/bundle
-echo "---xmonad"
-install ~/.xmobarrc rcfiles/.xmobarrc
-install ~/.xmonad rcfiles/.xmonad
 echo "---i3"
 install ~/.i3 rcfiles/i3
 echo "---git"
 install ~/.gitconfig rcfiles/.gitconfig
-install ~/.githudrc rcfiles/githudrc
 echo "---tmux"
 install ~/.tmux.conf rcfiles/.tmux.conf
 install ~/.tmux/plugins/tpm deps/tpm
-echo "---conky"
-install ~/.conkyrc rcfiles/.conkyrc
 echo "---python"
 install ~/.pythonrc.py rcfiles/.pythonrc.py
 install ~/.config/ipython/profile_default/ipython_config.py rcfiles/ipythonrc.py
@@ -98,35 +62,6 @@ install ~/.emacs.d rcfiles/emacs.d
 mkdir -p ~/.config/systemd/user
 cp resources/emacs.service ~/.config/systemd/user/emacs.service
 cp resources/ssh-agent.service ~/.config/systemd/user/ssh-agent.service
-
-echo "--installing light table config"
-install ~/.config/LightTable/User/user.behaviors light-table/user.behaviors
-install ~/.config/LightTable/User/user.keymap light-table/user.keymap
-
-#---sublime text
-echo "--installing ST3 config"
-#------snippets
-echo "---snippets"
-install ~/.config/sublime-text-3/Packages/User/coroutine.sublime-snippet sublime/coroutine.sublime-snippet
-install ~/.config/sublime-text-3/Packages/User/decorate_coroutine.sublime-snippet sublime/decorate_coroutine.sublime-snippet
-echo "---settings"
-install ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings sublime/Preferences.sublime-settings
-install ~/.config/sublime-text-3/Packages/User/Anaconda.sublime-settings sublime/Anaconda.sublime-settings
-install ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings sublime/Package\ Control.sublime-settings
-install ~/.config/sublime-text-3/Packages/User/GoSublime-Go.sublime-settings sublime/GoSublime-Go.sublime-settings
-install ~/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings sublime/GoSublime.sublime-settings
-install ~/.config/sublime-text-3/Packages/User/RustAutoComplete.sublime-settings sublime/RustAutoComplete.sublime-settings
-echo "---keybindings"
-install ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap sublime/Default\ \(Linux\).sublime-keymap
-echo "---build systems"
-echo "----python"
-install ~/.config/sublime-text-3/Packages/User/pyb-venv.sublime-build sublime/pyb-venv.sublime-build
-install ~/.config/sublime-text-3/Packages/User/python-single-file.sublime-build sublime/python-single-file.sublime-build
-install ~/.config/sublime-text-3/Packages/User/python-single-file-with-venv.sublime-build sublime/python-single-file-with-venv.sublime-build
-echo "----java"
-install ~/.config/sublime-text-3/Packages/User/mvn.sublime-build sublime/mvn.sublime-build
-echo "----js"
-install ~/.config/sublime-text-3/Packages/User/nodejs-single-file.sublime-build sublime/nodejs-single-file.sublime-build
 
 install ~/.rustfmt.toml rcfiles/.rustfmt.toml
 
@@ -140,30 +75,9 @@ install ~/.xsessionrc rcfiles/xsessionrc
 
 #---scripts
 echo "--installing scripts"
-install /usr/local/bin/statusbar bin/statusbar as_root
-install /usr/local/bin/xmonad-start bin/xmonad-start as_root
-install /usr/local/bin/xmobar-change-screen bin/xmobar-change-screen as_root
-install /usr/local/bin/pip-upgrade-all bin/pip-upgrade-all as_root
-install /usr/local/bin/go-upgrade bin/go-upgrade as_root
-install /usr/local/bin/pycharm bin/pycharm as_root
-install /usr/local/bin/light-table bin/light-table as_root
 install /usr/local/bin/pwn-docker bin/pwn-docker as_root
-install /usr/local/bin/render-wireless bin/render_wireless as_root
-install /usr/local/bin/passme bin/passme as_root
 install /usr/local/bin/byepass bin/byepass as_root
-install /usr/local/bin/drm bin/drm as_root
 install /usr/local/bin/i3lock.sh bin/i3lock.sh as_root
-install /usr/local/bin/lein bin/lein as_root
-install /usr/local/bin/pyb-vim bin/pyb-vim as_root
-install /usr/local/bin/toggle-touchpad bin/toggle-touchpad.sh as_root
 
 install /usr/local/bin/key_layout bin/key_layout as_root
 install /usr/local/bin/brightness-i3 bin/brightness-i3 as_root
-install /usr/local/bin/ensime bin/ensime as_root
-
-#---fonts
-if [[ $(uname) == 'Linux' ]]; then
-    install_source_code_pro
-else
-    echo "Please install source code pro yourself"
-fi

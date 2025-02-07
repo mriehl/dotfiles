e43fac3d20fa3787535682451a99bf20df457eac
Maximilien Riehl <max@riehl.io>
2025-02-07 17:34:35 +0100

    use ghostty and light mode

--- dot_config/nvim/lua/exact_plugins/rosepine.lua
+++ dot_config/nvim/lua/exact_plugins/rosepine.lua
@@ -7,7 +7,7 @@ return {
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			require("rose-pine").setup({
-				variant = "moon",
+				variant = "dawn",
 				-- highlight_groups = {
 				-- 	MiniCursorword = { underline = false, bg = "highlight_med" },
 				-- 	MiniCursorwordCurrent = {  underline = false },
